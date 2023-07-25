171756de8b290acef9ea4b131881deee2faf85f7
MateuszOlszanecki <mateuszolszanecki@gmail.com>
2023-07-25 07:19:40 +0200

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StaffListComponent } from './staff-list/staff-list.component';
-import { AddStaffMemberComponent } from './staff-list/add-staff-member/add-staff-member.component';
 import { StaffMemberTasksComponent } from './staff-member-tasks/staff-member-tasks.component';
-import { MoveTaskComponent } from './staff-member-tasks/move-task/move-task.component';
-import { AddTaskComponent } from './staff-member-tasks/add-task/add-task.component';
 import { StaffMemberComponent } from './staff-list/staff-member/staff-member.component';
 import { TaskComponent } from './staff-member-tasks/task/task.component';
 import { FormsModule } from '@angular/forms';
@@ -15,10 +12,7 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     StaffListComponent,
-    AddStaffMemberComponent,
     StaffMemberTasksComponent,
-    MoveTaskComponent,
-    AddTaskComponent,
     StaffMemberComponent,
     TaskComponent
   ],
