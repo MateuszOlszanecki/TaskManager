7f34f8c82a704f87fa42fb364716e08d2dffcf27
MateuszOlszanecki <mateuszolszanecki@gmail.com>
2023-08-01 11:46:35 +0200

    #

--- src/app/staff-member-tasks/move-task/search-result/search-result.component.ts
+++ src/app/staff-member-tasks/move-task/search-result/search-result.component.ts
@@ -4,8 +4,7 @@ import { StaffListService } from 'src/app/services/staff-list.service';
 
 @Component({
   selector: 'app-search-result',
-  templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.css']
+  templateUrl: './search-result.component.html'
 })
 export class SearchResultComponent {
   @Input() staff_member_searched!: StaffMember;
