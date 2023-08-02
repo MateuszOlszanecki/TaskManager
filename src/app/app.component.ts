60403e29756d6b7dcfc03f866cbce41d532f9ea9
MateuszOlszanecki <mateuszolszanecki@gmail.com>
2023-08-02 11:24:07 +0200

    wrongLinkErrorHandled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StaffListService } from './services/staff-list.service';
 import { TasksService } from './services/tasks.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,12 @@ import { TasksService } from './services/tasks.service';
 export class AppComponent implements OnInit{ 
 
   constructor(private staffListService: StaffListService,
-              private tasksService: TasksService) {}
+              private tasksService: TasksService,
+              private router: Router) {}
 
   ngOnInit() {
     this.staffListService.getStaffListFromDatabase();
     this.tasksService.getTasksFromDatabase();
+    this.router.navigate(['']);
   }
 }
