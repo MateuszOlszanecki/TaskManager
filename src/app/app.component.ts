552dd14d2a7370010bee1cf3ec8ae36814d33ba8
MateuszOlszanecki <mateuszolszanecki@gmail.com>
2023-08-02 11:27:05 +0200

    #

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit{ 
+export class AppComponent implements OnInit { 
 
   constructor(private staffListService: StaffListService,
               private tasksService: TasksService,
