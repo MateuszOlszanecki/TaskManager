5d85a2ff74f63e2c4f2c18cf7b3fa2b364f41ef9
MateuszOlszanecki <mateuszolszanecki@gmail.com>
2023-08-09 12:30:35 +0200

    CORSpolicyErrorHandled

--- back-end/src/main/java/com/TaskManager/backend/dao/StaffMemberRepository.java
+++ back-end/src/main/java/com/TaskManager/backend/dao/StaffMemberRepository.java
@@ -3,7 +3,9 @@ package com.TaskManager.backend.dao;
 import com.TaskManager.backend.entity.StaffMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(path = "staff-list")
+@CrossOrigin(origins = "http://localhost:4200")
 public interface StaffMemberRepository extends JpaRepository<StaffMember, Integer> {
 }
