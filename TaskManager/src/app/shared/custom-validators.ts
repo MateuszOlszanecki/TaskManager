bbf5b55153bb3942305b8f4a45769bc547ea3b4d
MateuszOlszanecki <mateuszolszanecki@gmail.com>
2023-08-17 13:44:07 +0200

    removedUnnecessaryCustomValidator

--- TaskManager/src/app/shared/custom-validators.ts
+++ TaskManager/src/app/shared/custom-validators.ts
@@ -1,5 +1,4 @@
 import { AbstractControl } from "@angular/forms";
-import { GlobalVariables } from "./global-variables";
 
 export class CustomValidators {    
     static searchValid(control: AbstractControl): {[s: string]: boolean} | null {
@@ -19,23 +18,5 @@ export class CustomValidators {
             return null;
         }
         return {'onlySpaces': false};
-    }
-
-    static maxLengthTextArea(control: AbstractControl): {[s: string]: boolean} | null {
-        let text: string = control.value.trim();
-        let maxLength = GlobalVariables.MAX_LENGTH_TEXT_AREA;
-        if(text.length <= maxLength) {
-            return null;
-        }
-        return {'maxLength': false};
-    }
-
-    static maxLengthInput(control: AbstractControl): {[s: string]: boolean} | null {
-        let text: string = control.value.trim();
-        let maxLength = GlobalVariables.MAX_LENGTH_INPUT;
-        if(text.length <= maxLength) {
-            return null;
-        }
-        return {'maxLength': false};
     }
 }
--- TaskManager/src/app/staff-list/edit-staff-member/edit-staff-member.component.ts
+++ TaskManager/src/app/staff-list/edit-staff-member/edit-staff-member.component.ts
@@ -44,9 +44,9 @@ export class EditStaffMemberComponent implements OnInit {
     }
     
     this.staffMemberForm = new FormGroup({
-      'name': new FormControl(name, [Validators.required, CustomValidators.onlySpaces, CustomValidators.maxLengthInput]),
-      'surname': new FormControl(surname, [Validators.required, CustomValidators.onlySpaces, CustomValidators.maxLengthInput]),
-      'position': new FormControl(position, [Validators.required, CustomValidators.onlySpaces, CustomValidators.maxLengthInput])
+      'name': new FormControl(name, [Validators.required, Validators.maxLength(GlobalVariables.MAX_LENGTH_INPUT), CustomValidators.onlySpaces]),
+      'surname': new FormControl(surname, [Validators.required, Validators.maxLength(GlobalVariables.MAX_LENGTH_INPUT), CustomValidators.onlySpaces]),
+      'position': new FormControl(position, [Validators.required, Validators.maxLength(GlobalVariables.MAX_LENGTH_INPUT), CustomValidators.onlySpaces])
     })
   }
 
